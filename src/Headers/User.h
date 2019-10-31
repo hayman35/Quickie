3b8a536d5a68376663df2497bef489ef5de482e7
Nirmal <nirmalvettiankal@gmail.com>
2019-10-31 15:18:07 -0400

    user header

--- src/Headers/User.h
+++ src/Headers/User.h
@@ -1,21 +1,31 @@
 //
 // Created by Kevin Johar on 2019-10-30.
 //
-
-using namespace std;
 #include <string.h>
-#include "./../Domain/GeographicCoordinate.cpp"
+#include "../Domain/GeographicCoordinate.cpp"
+#include "../Trip/Trip.cpp"
 
 #ifndef QUICKIE_USER_H
 #define QUICKIE_USER_H
 
-class UserInterface {
-    public:
-        GeographicCoordinate getStartLocation();
+class User {
     private:
-        string userId;
-        string oAuthToken;
-        GeographicCoordinate startLocation;
+        std::string userID;
+        std::string oAuthToken;
+        GeographicCoordinate* startLocation;
+        Trip* trip;
+
+    public:
+        User(std::string userID);
+        std::string getUserID();
+        void setUserID();
+        std::string getOAuthToken();
+        void setOAuthToken(std::string oAuthToken);
+        GeographicCoordinate getStartLocation();
+        void setStartLocation(GeographicCoordinate startLocation);
+        void addTrip(Trip trip);
+        Trip getTrip();
+        Trip removeTrip();
 };
 
 #endif //QUICKIE_USER_H
