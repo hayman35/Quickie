68666edd8caa6a343d109bd17981108424e7cfb8
Kevin Johar <kevin.s.johar@gmail.com>
2019-10-31 14:31:03 -0400

    Header folder created

--- src/User/UserInterface.cpp
+++ src/User/UserInterface.cpp
@@ -1,4 +1,14 @@
 //
 // Created by Kevin Johar on 2019-10-30.
 //
+#include "./../Domain/GeographicCoordinate.cpp"
 
+class UserInterface {
+    private:
+        string userId;
+        string oAuthToken;
+        GeographicCoordinate startLocation;
+
+    public:
+        GeographicCoordinate getStartLocation();
+};
