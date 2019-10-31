8ed7d7170449dd1445bab52f2f2bbe01dbdc176c
Nikka Velikonja <nvelikon@uwo.ca>
2019-10-31 15:13:20 -0400

    add GeographicCoordinate.h

---
+++ src/Headers/GeographicCoordinate.h
@@ -0,0 +1,24 @@
+#include <string.h>
+
+#ifndef QUICKIE_GEOGRAPHICCOORDINATE_H
+#define QUICKIE_GEOGRAPHICCOORDINATE_H
+
+class GeographicCoordinate {
+    private:
+        long latitude;
+        long longitude;
+
+    public:
+        GeographicCoordinate(long latitude, long longitude);
+
+        long getLatitude();
+
+        void setLatitude(long latitude);
+
+        long getLongitude();
+
+        void setLongitude(long longitude);
+
+};
+
+#endif //QUICKIE_GEOGRAPHICCOORDINATE_H
