8aca336699e1a639a93a7c8a611687998b9a50e6
Andrew Keyes <keyesandrew@live.com>
2019-10-31 10:07:47 -0400

    Different command types

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,9 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char** argv)
-{
-    Poco::MD5Engine md5;
-    Poco::DigestOutputStream ds(md5);
-    ds << "abcdefghijklmnopqrstuvwxyz";
-    ds.close();
-    cout << Poco::DigestEngine::digestToHex(md5.digest()) << endl;
 
-    UserFactory userFactory = new UserFactory();
+void requestRide(user){
     TripFactory tripFactory = new TripFactory();
-
-    //Get command
-    //request trip
 
     long startLat = 0;
     long startLong = 0;
@@ -29,11 +19,67 @@ int main(int argc, char** argv)
 
     GeographicCoordinate endLocation = new GeographicCoordinate(startLat, startLong);
 
-    User user = userFactory.createUser();
     Trip trip = tripFactory.create(user, user.getStartLocation(), endLocation)
     user.addTrip(trip);
-    trip.start();
+    string response = "";
+    cout << "confirm [Y]es, [n]o? ";
+    response << cin;
+    if(response == "Y"){
+        trip.start();
+        cout << "Trip Started!";
+    } else {
+        cout << "Trip cancelled" << endl;
+        user.removeTrip(Trip);
+    }
+}
+
+void setUberType(user){
+    string uberType = "";
+    cout << "Enter Uber Type (UberX, UberXL, UberBlack): ";
+    uberType << cin;
+    user.updateUberType(uberType);
+    cout << endl <<"Uber type updated";
+}
+
+void getCurrentETA(user){
+    Trip trip = user.getTrip();
+    cout << trip.getTimeEstimate() << endl;
+}
 
 
+int main(int argc, char** argv)
+{
+    Poco::MD5Engine md5;
+    Poco::DigestOutputStream ds(md5);
+    ds << "abcdefghijklmnopqrstuvwxyz";
+    ds.close();
+    cout << Poco::DigestEngine::digestToHex(md5.digest()) << endl;
+
+    UserFactory userFactory = new UserFactory();
+    User user = userFactory.createUser();
+
+    //Get command
+    //request trip
+
+    while(true){
+        cout << "Select command" << endl;
+        cout << "1) Request Ride" << endl;
+        cout << "2) Set Uber Type" << endl;
+        cout << "3) Get Current Ride ETA" << endl;
+
+        int response;
+        response << cin;
+        if(response != 1 && response != 2 && response != 3 && response != 4){
+            continue;
+        }
+        if(response == 1){
+            requestRide(user);
+        } else if(response == 2) {
+            setUberType(user);
+        } else {
+            getCurrentETA(user);
+        }
+
+    }
     return 0;
 }
