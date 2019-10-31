8b5182ed61f0661039986d91a36b36eabed3c7d4
Andrew Keyes <keyesandrew@live.com>
2019-10-31 10:19:47 -0400

    Add user / userfactory

--- src/User/User.cpp
+++ src/User/User.cpp
@@ -1,4 +1,42 @@
 //
 // Created by Kevin Johar on 2019-10-30.
 //
+#include <string.h>
+#include "GeographicCoordinate.h"
 
+class User {
+    private:
+        string userID;
+        string oAuthToken;
+        GeographicCoordinate startLocation;
+
+    public:
+        User(string userID){
+            this->userUD = userID;
+        }
+
+        string getUserID(){
+            return userID;
+        }
+
+        void setUserID(){
+            this->userID = userID;
+        }
+
+        string getOAuthToken(){
+            return oAuthToken;
+        }
+
+        void setOAuthToken(string oAuthToken){
+            this->oAuthToken = oAuthToken;
+        }
+
+        GeographicCoordinate getStartLocation(){
+            return startLocation;
+        }
+
+        void setStartLocation(GeographicCoordinate startLocation){
+            this->startLocation = startLocation;
+        }
+
+}
---
+++ src/User/UserFactory.cpp
@@ -0,0 +1,7 @@
+class UserFactory{
+
+    public:
+        User createUser(){
+            
+        }
+}
