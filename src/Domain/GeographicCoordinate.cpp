b5669fb99987959e0f8fa7ce34a3e8524c673786
Hayman <habdelj@uwo.ca>
2019-11-28 20:43:30 -0500

    Comments

--- src/Domain/GeographicCoordinate.cpp
+++ src/Domain/GeographicCoordinate.cpp
@@ -1,22 +1,48 @@
 #include "../Headers/GeographicCoordinate.h"
 
+/**
+ * @brief Construct a new Geographic Coordinate:: Geographic Coordinate object
+ * 
+ * @param latitude 
+ * @param longitude 
+ */
 GeographicCoordinate::GeographicCoordinate(long latitude, long longitude) {
-    this->latitude = latitude;
-    this->longitude = longitude;
+    this->latitude = latitude; /**< Sets the local variable*/
+    this->longitude = longitude; /**< Sets the local variable*/
 }
 
+/**
+ * @brief Getter method for the latitude 
+ * 
+ * @return long 
+ */
 long GeographicCoordinate::getLatitude(){
     return this->latitude;
 }
 
+/**
+ * @brief Setter method for Latitude 
+ * 
+ * @param latitude 
+ */
 void GeographicCoordinate::setLatitude(long latitude){
     this->latitude = latitude;
 }
 
+/**
+ * @brief Getter method for the Longitude 
+ * 
+ * @return long 
+ */
 long GeographicCoordinate::getLongitude(){
     return this->longitude;
 }
 
+/**
+ * @brief Setter method for the Longitude 
+ * 
+ * @param longitude 
+ */
 void GeographicCoordinate::setLongitude(long longitude){
     this->longitude = longitude;
 }
