4cd90c010121f45bd50785d4ca1726c105b6b97a
Nikka Velikonja <nvelikon@uwo.ca>
2019-11-28 20:38:03 -0500

    doxygen comments

--- src/Trip/TripFactory.cpp
+++ src/Trip/TripFactory.cpp
@@ -1,5 +1,13 @@
 #include "../Headers/TripFactory.h"
 
+/**
+ * @brief Factory for creating trips
+ * @authors Kevin, Andrew, Hayman, Nirmal
+ * @param user
+ * @param start
+ * @param end
+ * @return Trip* pointer to trip
+ */
 
 Trip* TripFactory::create(User user, GeographicCoordinate* start, GeographicCoordinate* end){
     Trip* trip = new UberTrip();
